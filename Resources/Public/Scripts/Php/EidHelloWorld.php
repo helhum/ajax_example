345c140ab12d313b756c27c771c32cc9c0ba99d8
Oliver Klee <typo3-coding@oliverklee.de>
2016-07-17 14:08:37 +0200

    [TASK] Unify copyright header and add a LICENSE

--- Resources/Public/Scripts/Php/EidHelloWorld.php
+++ Resources/Public/Scripts/Php/EidHelloWorld.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * This file is part of the Ajax example TYPO3 extension.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read
+ * LICENSE file that was distributed with this source code.
+ */
+
 defined('TYPO3_MODE') or die('Access denied.');
 
 echo 'Hello World';
